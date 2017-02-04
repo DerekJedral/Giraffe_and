ba9fa66315d8059a4b990ecabbf55c88ccd541c2
Derek Jedral <derekjedral@gmail.com>
2017-02-04 18:17:32 -0500

    Login Page, Employer Start

---
+++ app/src/main/java/com/example/derek/giraffe_android/EmployerActivity.java
@@ -0,0 +1,14 @@
+package com.example.derek.giraffe_android;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer_home);
+    }
+}
--- app/src/main/java/com/example/derek/giraffe_android/LoginActivity.java
+++ app/src/main/java/com/example/derek/giraffe_android/LoginActivity.java
@@ -1,13 +1,47 @@
 package com.example.derek.giraffe_android;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
+
+    Button employerButton;
+    Button employeeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        employerButton = (Button)findViewById(R.id.employer_button_login);
+        employeeButton = (Button)findViewById(R.id.employee_button_login);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        setupButtons();
+    }
+
+    private void setupButtons(){
+        final Intent switchToEmployer = new Intent(this, EmployerActivity.class);
+        final Intent switchToEmployee = new Intent(this, EmployerActivity.class);
+
+        employerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(switchToEmployer);
+            }
+        });
+        employeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(switchToEmployee);
+            }
+        });
     }
 }
