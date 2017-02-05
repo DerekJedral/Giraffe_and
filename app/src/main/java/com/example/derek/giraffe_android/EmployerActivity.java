e29b9db5badfa197a1f83ac0458fb94fc9f986bb
Derek Jedral <derekjedral@gmail.com>
2017-02-05 00:47:36 -0500

    Matcher almost done

--- app/src/main/java/com/example/derek/giraffe_android/EmployerActivity.java
+++ app/src/main/java/com/example/derek/giraffe_android/EmployerActivity.java
@@ -1,6 +1,7 @@
 package com.example.derek.giraffe_android;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -34,12 +35,15 @@ public class EmployerActivity extends FragmentActivity {
         }
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.fragment_holder, matcherFragment).commit();
-        Button b1 = (Button)findViewById(R.id.finder);
-        Button b2 = (Button)findViewById(R.id.matches);
+        final Button b1 = (Button)findViewById(R.id.finder);
+        final Button b2 = (Button)findViewById(R.id.matches);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                b1.setBackgroundColor(Color.parseColor("#999999"));
+                b2.setBackgroundColor(Color.parseColor("#444444"));
                 FragmentTransaction ft = fm.beginTransaction();
+                ft.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right);
                 ft.replace(R.id.fragment_holder, matcherFragment);
                 ft.commit();
             }
@@ -47,7 +51,10 @@ public class EmployerActivity extends FragmentActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                b2.setBackgroundColor(Color.parseColor("#999999"));
+                b1.setBackgroundColor(Color.parseColor("#444444"));
                 FragmentTransaction ft = fm.beginTransaction();
+                ft.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
                 ft.replace(R.id.fragment_holder, matchesFragment);
                 ft.commit();
             }
--- app/src/main/java/com/example/derek/giraffe_android/MatcherFragment.java
+++ app/src/main/java/com/example/derek/giraffe_android/MatcherFragment.java
@@ -1,12 +1,20 @@
 package com.example.derek.giraffe_android;
 
 
+import android.media.Image;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+import static android.view.MotionEvent.ACTION_UP;
 
 public class MatcherFragment extends Fragment {
 
@@ -15,6 +23,50 @@ public class MatcherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.matcher_fragment, container,
                 false);
+
+
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("myLog","entered onResume");
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+
+        ImageView scrollDownButton = (ImageView)getView().findViewById(R.id.scrollDown);
+        final ScrollView s = (ScrollView)getView().findViewById(R.id.mainScroll);
+        s.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        scrollDownButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_UP:
+                        s.fullScroll(View.FOCUS_DOWN);
+                }
+                return true;
+            }
+        });
+
+        ImageView scrollUpButton = (ImageView)getView().findViewById(R.id.scrollUp);
+        scrollUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s.fullScroll(View.FOCUS_UP);
+            }
+        });
+        ImageView profilePic = (ImageView)getView().findViewById(R.id.profilePicture);
+        profilePic.setHorizontalFadingEdgeEnabled(true);
+        profilePic.setFadingEdgeLength(displaymetrics.widthPixels);
+        //Log.d("myLog","height is "+ mainImage.getLayoutParams().height);
+        //mainImage.getLayoutParams().height= 1920;
+
     }
 }
