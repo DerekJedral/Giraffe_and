6e9da46150825f081ed026ca7f0abd98a9dce0a2
Sandy Qin <sandy_qin.3636@hotmail.com>
2017-02-04 19:40:39 -0500

    updated EmployeeActivity
    cleaned up EmployeeActivity - rid of comment
    made activity_employee_home
    added EmployeeActivity to AndroidManifest

--- app/src/main/java/com/example/derek/giraffe_android/EmployeeActivity.java
+++ app/src/main/java/com/example/derek/giraffe_android/EmployeeActivity.java
@@ -1,13 +1,10 @@
 package com.example.derek.giraffe_android;
 
-/**
- * Created by sandy on 2017-02-04.
- */
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class EmployeeActivity extends Activity{
+public class EmployeeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
