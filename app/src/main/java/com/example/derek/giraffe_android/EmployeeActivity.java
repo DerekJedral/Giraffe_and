eba642f7925c1f9d700f1c5da1219ce3fadb7ec4
Sandy Qin <sandy_qin.3636@hotmail.com>
2017-02-05 01:59:37 -0500

    updated buttons
    created profileSettings button that leads to profile edit page
    imported things into ProfileEditFragment
    changed tab names at top of employee home page
    changed button names (saveProfile)

--- app/src/main/java/com/example/derek/giraffe_android/EmployeeActivity.java
+++ app/src/main/java/com/example/derek/giraffe_android/EmployeeActivity.java
@@ -1,14 +1,47 @@
 package com.example.derek.giraffe_android;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class EmployeeActivity extends Activity {
-
+public class EmployeeActivity extends FragmentActivity {
+    ProfileEditFragment profileEditFragment;
+    Fragment settingFragment;
+    FragmentManager fm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_home);
+        fm  = this.getSupportFragmentManager();
+        profileEditFragment = new ProfileEditFragment();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (profileEditFragment == null) {
+            Log.d("Crash","profileEditFragment is null");
+        }
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.fragment_holder, profileEditFragment).commit();
+        final Button b1 = (Button)findViewById(R.id.profileSettings);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b1.setBackgroundColor(Color.parseColor("#999999"));
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right);
+                ft.replace(R.id.fragment_holder, profileEditFragment);
+                ft.commit();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/derek/giraffe_android/ProfileEditFragment.java
+++ app/src/main/java/com/example/derek/giraffe_android/ProfileEditFragment.java
@@ -1,11 +1,14 @@
 package com.example.derek.giraffe_android;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProfileEditFragment extends Fragment {
 
@@ -15,4 +18,5 @@ public class ProfileEditFragment extends Fragment {
                 false);
         return v;
     }
+
 }
