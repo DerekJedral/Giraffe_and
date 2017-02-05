396804596258ec66a6c6b2734763f93d38b4d449
Derek Jedral <derekjedral@gmail.com>
2017-02-04 20:16:41 -0500

    Added Fragments

---
+++ app/src/main/java/com/example/derek/giraffe_android/MatcherFragment.java
@@ -0,0 +1,20 @@
+package com.example.derek.giraffe_android;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MatcherFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.matcher_fragment, container,
+                false);
+        return v;
+    }
+}
